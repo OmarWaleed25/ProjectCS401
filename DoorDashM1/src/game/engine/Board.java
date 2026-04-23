3863f803e8f6208b17b2aa514424333ec1769996
OmarWaleed25 <omarwaleed2572@gmail.com>
2026-04-23 12:21:26 +0200

    added methods to board
    indexToRowCol(int index)
    Cell getCell(int index)
    void setCell(int index, Cell cell)
    incomplete void initializeBoard(ArrayList<Cell> specialCells)

--- DoorDashM1/src/game/engine/Board.java
+++ DoorDashM1/src/game/engine/Board.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import game.engine.cards.Card;
 import game.engine.cells.*;
 import game.engine.monsters.Monster;
+import game.engine.Constants;
 
 public class Board {
 	private Cell[][] boardCells;
@@ -42,4 +43,70 @@ public class Board {
 	public static void setCards(ArrayList<Card> cards) {
 		Board.cards = cards;
 	}
+	
+	private int[] indexToRowCol(int index){
+		int[] sol = new int[2];
+		sol[0] = index/10;
+		if(sol[0]%2==0)
+			sol[1] = index%10;
+		else
+			sol[1] = 9 - index%10;
+		return sol;
+	}
+	
+	private Cell getCell(int index){
+		int [] ind = indexToRowCol(index);
+		return boardCells[ind[0]][ind[1]];
+	}
+	
+	private void setCell(int index, Cell cell){
+		int [] ind = indexToRowCol(index);
+		boardCells[ind[0]][ind[1]] = cell;
+	}
+	
+	
+	void initializeBoard(ArrayList<Cell> specialCells){
+		ArrayList<DoorCell> doors = new ArrayList<>();
+		ArrayList<ConveyorBelt> belts = new ArrayList<>();
+		ArrayList<ContaminationSock> socks = new ArrayList<>();
+		
+		for(int i=0;i<specialCells.size();i++){
+			if(specialCells.get(i) instanceof DoorCell) doors.add((DoorCell) specialCells.get(i));
+			
+			else if(specialCells.get(i) instanceof ConveyorBelt) belts.add((ConveyorBelt) specialCells.get(i));
+			
+			else socks.add((ContaminationSock) specialCells.get(i));
+		}
+		int d =0; // index for the doors list
+		int b =0; // index for the belts list
+		int s = 0; // index for the socks list
+		int bc =0; // index for the list of constant positions of 
+		int sc =0; // index for the list of constant positions of 
+		
+		for(int i = 0;i < 100;i++){
+			if(i%2==1){ 
+				setCell(i,doors.get(d));
+				d++;
+			}
+			else{
+				if(i==Constants.CONVEYOR_CELL_INDICES[bc]){
+
+
+				}
+			}
+			
+		
+		
+		}
+	
+	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 }
