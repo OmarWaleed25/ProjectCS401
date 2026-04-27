709089185d5f0c30cf634027973020187f0c5dc5
omarhilal1 <omarhilal0777@gmail.com>
2026-04-27 22:52:35 +0300

    edited game class

--- DoorDashM1/src/game/engine/Game.java
+++ DoorDashM1/src/game/engine/Game.java
@@ -3,10 +3,11 @@ package game.engine;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
-
+import java.util.Random;
+import game.engine.exceptions.*;
 import game.engine.dataloader.DataLoader;
 import game.engine.monsters.*;
-
+import game.engine.cells.*;
 public class Game {
 	private Board board;
 	private ArrayList<Monster> allMonsters; 
@@ -15,13 +16,13 @@ public class Game {
 	private Monster current;
 	
 	public Game(Role playerRole) throws IOException {
+		
 		this.board = new Board(DataLoader.readCards());
-		
 		this.allMonsters = DataLoader.readMonsters();
-		
 		this.player = selectRandomMonsterByRole(playerRole);
 		this.opponent = selectRandomMonsterByRole(playerRole == Role.SCARER ? Role.LAUGHER : Role.SCARER);
 		this.current = player;
+		
 	}
 	
 	public Board getBoard() {
@@ -56,4 +57,74 @@ public class Game {
 	    		.orElse(null);
 	}
 	
+	private Monster getCurrentOpponent(){
+		if(getCurrent()==getPlayer())
+			return getOpponent();
+		else
+			return getPlayer();
+	}
+	
+	private int rollDice(){
+		Random random = new Random();
+		int x = random.nextInt(6) + 1;
+		return x;
+	}
+	
+	public void usePowerup() throws OutOfEnergyException{
+		if ( this.getCurrent().getEnergy() >= Constants.POWERUP_COST ){
+			this.getCurrent().executePowerupEffect( this.getCurrentOpponent() );
+		}else{
+			throw new OutOfEnergyException ("Not Enough Energy for Power Up");
+		}
+	}
+	
+	public void playTurn() throws InvalidMoveException{
+		if(getCurrent().isFrozen()){
+			getCurrent().setFrozen(false);
+		}else{
+			int move = rollDice();
+			if(move+getCurrent().getPosition() == getOpponent().getPosition()){
+				throw new InvalidMoveException("Invalid move attempted");
+			}else{
+				getCurrent().move(move);
+				switchTurn();
+			}
+		}
+	}
+	
+	private void switchTurn(){
+		if(getCurrent() == getPlayer()){
+			setCurrent(getOpponent());
+		}else{
+			setCurrent(getPlayer());
+		}
+	}
+	
+	private boolean checkWinCondition(Monster monster){
+		if(monster.getEnergy() >= 1000 && monster.getPosition() == 99)
+			return true;
+		else
+			return false;
+	}
+	
+	public Monster getWinner(){
+		if(checkWinCondition(getCurrent())){
+			return getCurrent();
+		}
+		else if(checkWinCondition(getOpponent())){
+			return getOpponent();
+		}
+		else{
+			return null;
+		}
+	}
+	
+	public void setStationedMonsters(ArrayList<Monster> stationedMonsters){
+		setStationedMonsters(stationedMonsters);
+	}
+	
+	public void initializeBoard(ArrayList<Cell> specialCells){
+		initializeBoard(specialCells);
+	}
+	 
 }
