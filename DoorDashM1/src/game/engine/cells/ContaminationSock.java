da5743cbf9cdc758390c9ee0f9ba8dede712b71f
hassanooii <134282727+hassanooii@users.noreply.github.com>
2026-04-30 13:35:14 +0300

    18 f

--- DoorDashM1/src/game/engine/cells/ContaminationSock.java
+++ DoorDashM1/src/game/engine/cells/ContaminationSock.java
@@ -4,17 +4,21 @@ import game.engine.Constants;
 import game.engine.interfaces.CanisterModifier;
 import game.engine.monsters.Monster;
 
-public class ContaminationSock extends TransportCell{
+public class ContaminationSock extends TransportCell implements CanisterModifier {
 
-	public ContaminationSock(String name, int effect) {
-		super(name, effect);
-	}
-	public void transport(Monster landingMonster) {
+    public ContaminationSock(String name, int effect) {
+        super(name, effect);
+    }
+
+    public void transport(Monster landingMonster) {
         int newPosition = landingMonster.getPosition() + this.getEffect();
         landingMonster.setPosition(newPosition);
-        landingMonster.alterEnergy(Constants.SLIP_PENALTY);
+        modifyCanisterEnergy(landingMonster, Constants.SLIP_PENALTY);
+    }
+
+    @Override
+    public void modifyCanisterEnergy(Monster monster, int canisterValue) {
+        monster.alterEnergy(-Math.abs(canisterValue));
     }
 }
-
-
 
